f9c0fe72273e11fdf0b92df137f8aa9bfe38cbd1
janchundia <janchundia@guayatuna.com.ec>
2025-06-23 10:27:38 -0500

    agregar los cors

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import usersRoutes from "./routes/users.routes.js";
 import authRoutes from './routes/auth.routes.js';
 import morgan from "morgan";
+import cors from 'cors';
 import { PORT } from "./config.js";
 import swaggerUi from "swagger-ui-express"; // ✅ usar import
 import swaggerSpec from "./swagger.js";      // ✅ asegúrate de tener este archivo
@@ -9,6 +10,14 @@ import swaggerSpec from "./swagger.js";      // ✅ asegúrate de tener este arc
 const app = express();
 
 app.use(morgan("dev"));
+
+// Configuración básica (permitir todas las solicitudes desde cualquier origen)
+// app.use(cors({
+//     origin: 'http://localhost:3000', // cambia por el dominio de tu frontend
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     credentials: true
+//   }));
+app.options('*', cors()); // habilita preflight para todas las rutas
 
 // middlewares
 app.use(express.json());
