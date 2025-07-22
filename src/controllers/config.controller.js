bbc9d29f128b0d05de02f3aae60ca02ed44f10b7
janchundia <janchundia@guayatuna.com.ec>
2025-07-22 08:55:37 -0500

    Feat: Creacion de endpoint de maestros

--- src/controllers/config.controller.js
+++ src/controllers/config.controller.js
@@ -12,7 +12,38 @@ export const getBloodTypes = async (req, res) => {
 
 export const getDocumentTypes = async (req, res) => {
   try{
-    const response = await pool.query("SELECT * FROM config.blood_type ORDER BY id ASC");
+    const response = await pool.query("SELECT * FROM config.document_type where status = 1 ORDER BY id ASC");
+    res.status(200).json(response.rows);
+  }  catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+
+};
+
+export const getDentists = async (req, res) => {
+  try{
+    const response = await pool.query("SELECT * FROM config.dentists ORDER BY id ASC");
+    res.status(200).json(response.rows);
+  }  catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+
+};
+
+export const getOffices = async (req, res) => {
+  try{
+    const response = await pool.query("SELECT * FROM config.offices ORDER BY id ASC");
+    res.status(200).json(response.rows);
+  }  catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+
+};
+
+
+export const getAppointments = async (req, res) => {
+  try{
+    const response = await pool.query("SELECT * FROM office.appointments ORDER BY id ASC");
     res.status(200).json(response.rows);
   }  catch (error) {
     return res.status(500).json({ error: error.message });
--- src/routes/config.routes.js
+++ src/routes/config.routes.js
@@ -1,6 +1,10 @@
 import { Router } from "express";
 import {
-  getBloodTypes
+  getBloodTypes,
+  getDocumentTypes,
+  getDentists,
+  getOffices,
+  getAppointments
 } from "../controllers/config.controller.js";
 import { verificarToken } from '../helpers/jwt.js';
 
@@ -11,8 +15,8 @@ const router = Router();
 /**
  * @swagger
  * tags:
- *   name: BloodType
- *   description: Admin BloodType
+ *   name: Master
+ *   description: Admin Master
  */
 
 /**
@@ -20,23 +24,60 @@ const router = Router();
  * /bloodType:
  *   get:
  *     summary: Obtener todos los tipos de sangre
- *     tags: [BloodType]
+ *     tags: [Master]
  *     responses:
  *       200:
  *         description: Lista de tipos de sangre
  */
 router.get("/bloodType",verificarToken,  getBloodTypes);
 
-// /**
-//  * @swagger
-//  * /documentType:
-//  *   get:
-//  *     summary: Obtener todos los tipos de documentos
-//  *     tags: [DocumentType]
-//  *     responses:
-//  *       200:
-//  *         description: Lista de tipos de documentos
-//  */
-// router.get("/documentType",verificarToken,  getDocumentTypes);
+/**
+ * @swagger
+ * /documentType:
+ *   get:
+ *     summary: Obtener todos los tipos de documentos
+ *     tags: [Master]
+ *     responses:
+ *       200:
+ *         description: Lista de tipos de documentos
+ */
+router.get("/documentType",verificarToken,  getDocumentTypes);
+
+/**
+ * @swagger
+ * /dentists:
+ *   get:
+ *     summary: Obtener todos los Dentistas
+ *     tags: [Master]
+ *     responses:
+ *       200:
+ *         description: Lista de Dentistas
+ */
+router.get("/dentists",verificarToken,  getDentists);
+
+/**
+ * @swagger
+ * /offices:
+ *   get:
+ *     summary: Obtener todos los Offices
+ *     tags: [Master]
+ *     responses:
+ *       200:
+ *         description: Lista de Offices
+ */
+router.get("/offices",verificarToken,  getOffices);
+
+
+/**
+ * @swagger
+ * /appointments:
+ *   get:
+ *     summary: Obtener todos los Appointments
+ *     tags: [Master]
+ *     responses:
+ *       200:
+ *         description: Lista de Appointments
+ */
+router.get("/appointments",verificarToken,  getAppointments);
 
 export default router;
