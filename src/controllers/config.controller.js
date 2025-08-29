4ad7d13cc9c0dc90679974dbaaa047fd731064c7
EnriqueOlvera70 <146570482+EnriqueOlvera70@users.noreply.github.com>
2025-08-29 08:30:25 -0500

    feat: adición de consulta resumida de estadisticas (#17)
    * fix: corregir endpoint de creación historia clinica attachments
    
    * fix: corregir endpoint de creación historia clinica attachments separar parametros de body
    
    * feat: adición de consulta resumida de estadisticas
    
    ---------
    
    Co-authored-by: Pio Enrique Olvera briones <polvera@salicadelecuador.com.ec>

--- src/controllers/config.controller.js
+++ src/controllers/config.controller.js
@@ -60,3 +60,42 @@ export const getMaritalStatus= async (req, res) => {
   }
 
 };
+
+export const getEstadisticas = async (req, res) => {
+  try{
+    const payload = decodeToken(req.headers.authorization);
+    if (!payload) {
+        return res.status(401).json({ message: 'Invalid or expired token' });
+    }
+
+    let query = `
+    select sum(cantidadPatient) cantidadPatient,sum(cantidadCitas) cantidadCitas, sum(cantidadStock) cantidadStock, sum(cantidadProveedores) cantidadProveedores
+    from  (
+      select count(id) cantidadPatient, 0 cantidadCitas, 0 cantidadStock, 0 cantidadProveedores 
+      from office.patients
+      where status=1
+      and tenant_id=$1
+      union
+      select 0 cantidadPatient, count(id) cantidadCitas, 0 cantidadStock, 0 cantidadProveedores 
+      from office.appointments
+      where appointment_time::date=CURRENT_DATE
+      and tenant_id=$1
+      union
+      select 0 cantidadPatient, 0 cantidadCitas, count(stock_id) cantidadStock, 0 cantidadProveedores
+      from inventory.current_stock_view
+      where tenant_id=$1
+      union
+      select 0 cantidadPatient, 0 cantidadCitas, 0 cantidadStock, count(supplier_id) cantidadProveedores
+      from inventory.suppliers
+      where status=true
+      and tenant_id=$1
+    ) estadisticas`;
+
+    const response = await pool.query(query, [payload.tenant_id]);
+
+    res.status(200).json(response.rows);
+  }  catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+
+};
